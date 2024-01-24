d1137cda4d1a39488ff824f751a5bf78b073b355
Vladislav Filatov <v.filatov@softchamber.ru>
2024-01-24 20:16:54 +0400

    Retrieving data from elements and calling the MessageBox

--- StudyWpfProject/LoginWindow.xaml.cs
+++ StudyWpfProject/LoginWindow.xaml.cs
@@ -23,5 +23,37 @@ namespace StudyWpfProject
         {
             InitializeComponent();
         }
+
+
+
+        private void TestClick(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button is not null)
+            {
+                MessageBox.Show(button.Content.ToString());
+            }
+        }
+
+        private void EnterClick(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Text?", "Hello message", MessageBoxButton.YesNo, MessageBoxImage.Asterisk);
+            if (result == MessageBoxResult.Yes)
+            {
+                MessageBox.Show("ДА");
+            }
+            if (result == MessageBoxResult.No)
+            {
+                MessageBox.Show("НЕТ");
+            }
+
+            //string? login = loginTextBox.Text;
+            //string? password = passwordTextBox.Text;
+            //if (!string.IsNullOrEmpty(login) && !string.IsNullOrEmpty(password) && password == "1234")
+            //{
+            //    //MessageBox.Show($"ПРИВЕТ {login}");
+
+            //}
+        }
     }
 }
